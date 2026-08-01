82eb5fc98da07c8bc0f6d6006cfb8ffee580c887
lostvalkrie <lostvalkrie08@gmail.com>
2026-08-01 19:41:13 +0300

    Add isWorkingAgePerson funcion

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function isWorkingAgePerson(age) {
+  return age >= 16 && age <= 64;
+}
+
+console.log(isWorkingAgePerson(20));
+console.log(isWorkingAgePerson(4));
+console.log(isWorkingAgePerson(88));
